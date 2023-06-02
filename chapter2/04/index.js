4f25a85f62e9c1a5e1ae4546eda241b89bf3b64b
bejewel-seungtae <seungtae@bejewel.kr>
2023-06-02 22:14:46 +0900

    Feat : 5초마다 데이터 변경

---
+++ chapter2/04/index.js
@@ -0,0 +1,28 @@
+import todosView from "./view/todosView";
+import counterView from "./view/counterView";
+import filtersView from "./view/filtersView";
+
+import registry from "./registry";
+import getTodos from "./getTodos";
+
+registry.add("todos", todosView);
+registry.add("counter", counterView);
+registry.add("filters", filtersView);
+
+const state = {
+  todos: getTodos(),
+  currentFilter: "All",
+};
+
+const render = () => {
+  window.requestAnimationFrame(() => {
+    const main = document.querySelector(".todoapp");
+    const newMain = registry.renderRoot(main, state);
+    main.replaceWith(newMain);
+  });
+};
+
+setInterval(() => {
+  state.todos = getTodos();
+  render();
+}, 5000);
