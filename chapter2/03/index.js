c3c7a46eb62723e3aaf03bef760dc8e001f93b7d
bejewel-seungtae <seungtae@bejewel.kr>
2023-06-02 22:12:45 +0900

    Feat : registry 를 활용하여 렌더링 자동화

---
+++ chapter2/03/index.js
@@ -0,0 +1,21 @@
+import todosView from "./view/todosView";
+import counterView from "./view/counterView";
+import filtersView from "./view/filtersView";
+
+import registry from "./registry";
+import getTodos from "./getTodos";
+
+registry.add("todos", todosView);
+registry.add("counter", counterView);
+registry.add("filters", filtersView);
+
+const state = {
+  todos: getTodos(),
+  currentFilter: "All",
+};
+
+window.requestAnimationFrame(() => {
+  const main = document.querySelector(".todoapp");
+  const newMain = registry.renderRoot(main, state);
+  main.replaceWith(newMain);
+});
